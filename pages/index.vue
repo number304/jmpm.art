27cbd4a4dacfe7807bc5c449ada17e9c6c7827d5
number304 <jmpmateo3042013@gmail.com>
2021-06-04 20:45:41 -0500

    created first post card

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,24 @@
 <template>
   <v-container>
-    <ul>
-      <li v-for="post in posts" :key="post.id">
-        {{ post.title }}, por {{ post.primary_author.name }}
-      </li>
-    </ul>
+    <h2 class="text-h4 text-center mt-6 mb-2">
+      Mis últimos posts con tips para emprender online
+    </h2>
+    <div style="max-width: 1000px" class="mx-auto d-flex">
+      <v-card elevation="0" width="392" class="pa-4">
+        <h3 class="pa-4 pb-0">{{ posts[0].title }}</h3>
+        <v-card-subtitle
+          >Por {{ posts[0].primary_author.name }}</v-card-subtitle
+        >
+        <v-img
+          cover
+          class="mx-4"
+          height="250px"
+          :src="posts[0].feature_image"
+        ></v-img>
+        <v-card-text>{{ posts[0].meta_description }}</v-card-text>
+      </v-card>
+      <v-divider vertical class="ma-2"></v-divider>
+    </div>
   </v-container>
 </template>
 
