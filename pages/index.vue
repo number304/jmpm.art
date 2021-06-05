01beec687665f02d8f78829a70dcbdedf54a4c01
number304 <jmpmateo3042013@gmail.com>
2021-06-05 15:34:44 -0500

    second post done

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-container>
-    <h2 class="text-h4 text-center mt-6 mb-2">
+  <v-container class="px-0" style="min-width: 100%; max-width: 1000px">
+    <h2 class="text-h4 text-center mt-6 mb-4 mx-auto" style="max-width: 600px">
       Mis últimos posts con tips para emprender online
     </h2>
     <div style="max-width: 1000px" class="mx-auto d-flex">
@@ -18,6 +18,24 @@
         <v-card-text>{{ posts[0].meta_description }}</v-card-text>
       </v-card>
       <v-divider vertical class="ma-2"></v-divider>
+      <div>
+        <v-card elevation="0" class="pa-4 d-flex">
+          <div style="width: 60%">
+            <h3 class="pa-4 pb-0">{{ posts[1].title }}</h3>
+            <v-card-subtitle
+              >Por {{ posts[1].primary_author.name }}</v-card-subtitle
+            >
+            <v-card-text>{{ posts[1].meta_description }}</v-card-text>
+          </div>
+          <v-img
+            cover
+            class="mx-1"
+            style="width: 200px"
+            height="200px"
+            :src="posts[1].feature_image"
+          ></v-img>
+        </v-card>
+      </div>
     </div>
   </v-container>
 </template>
