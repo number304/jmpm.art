c3977c53f20402cbe7b3e2638bebbfe26b4a25fc
number304 <jmpmateo3042013@gmail.com>
2021-06-05 20:18:25 -0500

    featured blogs done and responsive

--- pages/index.vue
+++ pages/index.vue
@@ -1,58 +1,99 @@
 <template>
-  <v-container class="px-0" style="min-width: 100%; max-width: 1000px">
-    <h2 class="text-h4 text-center mt-6 mb-4 mx-auto" style="max-width: 600px">
-      Mis últimos posts con tips para emprender online
-    </h2>
-    <div style="max-width: 1000px" class="mx-auto d-flex">
-      <v-card elevation="0" width="392" class="pa-4">
-        <h3 class="pa-4 pb-0">{{ posts[0].title }}</h3>
-        <v-card-subtitle
-          >Por {{ posts[0].primary_author.name }}</v-card-subtitle
+  <div>
+    <v-container class="px-0" style="min-width: 100%; max-width: 1000px">
+      <h2
+        class="text-h4 text-center mt-6 mb-4 mx-auto font-weight-medium"
+        style="max-width: 600px"
+      >
+        Mis últimos posts con tips para emprender online
+      </h2>
+      <div
+        style="max-width: 1000px"
+        class="mx-auto d-flex flex-column flex-md-row"
+      >
+        <v-card
+          elevation="0"
+          :width="$vuetify.breakpoint.xsOnly ? '' : '392'"
+          class="pa-4"
+          :class="$vuetify.breakpoint.smAndDown ? 'mx-auto' : ''"
         >
-        <v-img
-          cover
-          class="mx-4"
-          height="250px"
-          :src="posts[0].feature_image"
-        ></v-img>
-        <v-card-text>{{ posts[0].excerpt }}</v-card-text>
-      </v-card>
-      <v-divider vertical class="ma-2"></v-divider>
-      <div>
-        <v-card elevation="0" class="pa-4 d-flex">
-          <div style="width: 60%">
-            <h3 class="pa-4 pb-0">{{ posts[1].title }}</h3>
-            <v-card-subtitle
-              >Por {{ posts[1].primary_author.name }}</v-card-subtitle
-            >
-            <v-card-text>{{ posts[1].excerpt }}</v-card-text>
-          </div>
+          <h3 class="pa-4 pb-0">{{ posts[0].title }}</h3>
+          <v-card-subtitle
+            >Por {{ posts[0].primary_author.name }}</v-card-subtitle
+          >
           <v-img
             cover
-            class="mx-1"
-            style="width: 200px"
-            height="200px"
-            :src="posts[1].feature_image"
+            class="mx-4"
+            height="250px"
+            :src="posts[0].feature_image"
           ></v-img>
+          <v-card-text>{{ posts[0].excerpt }}</v-card-text>
         </v-card>
-        <v-divider></v-divider>
-        <div class="d-flex">
-          <v-card elevation="0" class="pa-4" style="width: 48%">
-            <h3 class="pa-4 pb-0">{{ posts[2].title }}</h3>
-            <v-card-text>{{ posts[2].excerpt }}</v-card-text>
+        <v-divider
+          v-if="$vuetify.breakpoint.mdAndUp"
+          vertical
+          class="ma-2"
+        ></v-divider>
+        <v-divider
+          v-if="$vuetify.breakpoint.smAndDown"
+          class="mx-4"
+        ></v-divider>
+        <div>
+          <v-card
+            elevation="0"
+            :class="$vuetify.breakpoint.smAndDown ? 'mx-auto' : ''"
+            class="pa-4 d-flex flex-column flex-sm-row"
+          >
+            <div :style="$vuetify.breakpoint.mdAndUp ? 'width: 60%' : ''">
+              <h3 class="pa-4 pb-0">{{ posts[1].title }}</h3>
+              <v-card-subtitle
+                >Por {{ posts[1].primary_author.name }}</v-card-subtitle
+              >
+              <v-card-text>{{ posts[1].excerpt }}</v-card-text>
+            </div>
+            <v-img
+              cover
+              class="mx-1"
+              :width="$vuetify.breakpoint.xsOnly ? '' : '200'"
+              height="200"
+              :src="posts[1].feature_image"
+            ></v-img>
           </v-card>
-          <v-divider vertical class="ma-2"></v-divider>
-          <v-card elevation="0" class="pa-4" style="width: 48%">
-            <h3 class="pa-4 pb-0">{{ posts[3].title }}</h3>
-            <v-card-text>{{ posts[3].excerpt }}</v-card-text>
-          </v-card>
+          <v-divider class="mx-4"></v-divider>
+          <div class="d-flex flex-column flex-sm-row">
+            <v-card
+              elevation="0"
+              class="pa-4"
+              :style="$vuetify.breakpoint.smAndUp ? 'width: 48%' : ''"
+            >
+              <h3 class="pa-4 pb-0">{{ posts[2].title }}</h3>
+              <v-card-text>{{ posts[2].excerpt }}</v-card-text>
+            </v-card>
+            <v-divider
+              v-if="$vuetify.breakpoint.smAndUp"
+              vertical
+              class="ma-2"
+            ></v-divider>
+            <v-divider
+              v-if="$vuetify.breakpoint.xsOnly"
+              class="mx-4"
+            ></v-divider>
+            <v-card
+              elevation="0"
+              class="pa-4"
+              :style="$vuetify.breakpoint.smAndUp ? 'width: 48%' : ''"
+            >
+              <h3 class="pa-4 pb-0">{{ posts[3].title }}</h3>
+              <v-card-text>{{ posts[3].excerpt }}</v-card-text>
+            </v-card>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="d-flex justify-center">
-      <v-btn color="primary mt-1">Ver todo</v-btn>
-    </div>
-  </v-container>
+      <div class="d-flex justify-center">
+        <v-btn color="primary mt-1">Ver todo</v-btn>
+      </div>
+    </v-container>
+  </div>
 </template>
 
 <script>
