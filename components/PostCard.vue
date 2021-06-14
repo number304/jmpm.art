ccd985b75fbfd6935228413739a816db0159437c
number304 <jmpmateo3042013@gmail.com>
2021-06-13 20:02:04 -0500

    added grid system to blog page

---
+++ components/PostCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-card elevation="0" class="px-2">
+    <v-img :src="post.feature_image"></v-img>
+    <h3 class="mt-4">{{ post.title }}</h3>
+    <p>{{ post.excerpt }}</p>
+    <div class="d-flex">
+      <v-avatar size="40" class="mr-2" style="border-radius: 50%">
+        <v-img :src="post.primary_author.profile_image"></v-img>
+      </v-avatar>
+      <div>
+        <p class="mb-0 text-body-2 font-weight-medium">
+          {{ post.primary_author.name }}
+        </p>
+        <p class="text-body-2">
+          {{ formatDate(post) }} • Lectura de {{ post.reading_time }} min.
+        </p>
+      </div>
+    </div>
+  </v-card>
+</template>
+
+<script>
+import { formatDate } from '@/api/posts'
+
+export default {
+  props: {
+    post: {
+      type: Object,
+      default: () => {},
+    },
+  },
+
+  methods: {
+    formatDate,
+  },
+}
+</script>
+
+<style></style>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -3,25 +3,11 @@
     <h1 class="text-center">¡Emprende en Internet de la mejor manera!</h1>
     <p class="text-overline text-center">Con este contenido de primera</p>
 
-    <v-card elevation="0" class="px-2">
-      <v-img :src="posts[0].feature_image"></v-img>
-      <h3 class="mt-4">{{ posts[0].title }}</h3>
-      <p>{{ posts[0].excerpt }}</p>
-      <div class="d-flex">
-        <v-avatar size="40" class="mr-2" style="border-radius: 50%">
-          <v-img :src="posts[0].primary_author.profile_image"></v-img>
-        </v-avatar>
-        <div>
-          <p class="mb-0 text-body-2 font-weight-medium">
-            {{ posts[0].primary_author.name }}
-          </p>
-          <p class="text-body-2">
-            {{ formatDate(posts[0]) }} • Lectura de
-            {{ posts[0].reading_time }} min.
-          </p>
-        </div>
-      </div>
-    </v-card>
+    <v-row>
+      <v-col v-for="(post, index) in posts" :key="index" cols="12" sm="6">
+        <PostCard :post="post" />
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
