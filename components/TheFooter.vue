9387aa716d9e8f13016efa60840dc7f514261ff4
number304 <jmpmateo3042013@gmail.com>
2021-06-15 14:35:34 -0500

    added urls to my linkedin and github in footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -19,6 +19,17 @@
             >Contacto</nuxt-link
           >
         </div>
+        <div class="mt-1">
+          <a
+            href="https://www.linkedin.com/in/jose-miguel-pedraza-mateo-n304/"
+            target="_blank"
+          >
+            <img class="mx-2" src="@/static/linkedIn.svg" alt="Linkedin Logo" />
+          </a>
+          <a href="https://github.com/number304" target="_blank">
+            <img class="mx-2" src="@/static/github.svg" alt="Github Logo" />
+          </a>
+        </div>
       </v-card>
     </v-footer>
   </div>
